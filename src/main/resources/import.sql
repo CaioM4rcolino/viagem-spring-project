02f92c074ea3ff6574ff415b2fd7527c3b80b39d
CaioM4rcolino <caioaugustowill@gmail.com>
2021-04-29 10:41:48 -0300

    Primeira versão

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO estado values(1,'São Paulo', 'SP');
+INSERT INTO estado values(2,'Minas Gerais', 'MG');
+INSERT INTO estado values(3,'Rio de Janeiro', 'RJ'); 
+INSERT INTO estado values(4,'Santa Catarina', 'SC');
+INSERT INTO estado values(5,'Espírito Santo', 'ES');
